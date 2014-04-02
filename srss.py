d1a83fd200900b3587b037ca7fab27189c28ecca
Joseph Lansdowne <J49137@gmail.com>
2014-04-02 22:04:56 +0100

    srss: documentation (release 2)

--- srss.py
+++ srss.py
@@ -3,7 +3,7 @@
 This is an early release, and is buggy, undocumented and lacking in features.
 
 Python version: 3.
-Release: 1.
+Release: 2.
 
 Licensed under the GNU General Public License, version 3; if this was not
 included, you can find it here:
@@ -13,8 +13,6 @@ included, you can find it here:
 
 """
 TODO:
-    rename
-    doc
     feed: support optional attrs; __*item__() with validation
     add items to a feed
         set feed pubDate, lastBuildDate
@@ -23,6 +21,8 @@ TODO:
             source, enclosure, category aren't text
         expiry options: remove old items by age/cutoff date/number of items
             can return removed items/store in another file/file object
+    custom temp dir
+    support file obj instead of filename, somehow
 """
 
 import time
@@ -40,9 +40,8 @@ FEED_DEFAULTS = {'docs': 'http://blogs.law.harvard.edu/tech/rss'}
 def _prep_item (item):
     item = dict(item)
     if 'title' not in item and 'description' not in item:
-        raise ValueError('feed item must have title or '
-                            'description')
-    date = item['pubDate'] if 'pupDate' in item else time.localtime()
+        raise ValueError('feed item must have title or description')
+    date = item['pubDate'] if 'pubDate' in item else time.localtime()
     if isinstance(date, time.struct_time):
         date = time.strftime(FEED_DATE_FORMAT, date)
     if 'guid' not in item:
@@ -52,12 +51,27 @@ def _prep_item (item):
 
 
 class Feed (dict):
+    """RSS feed, supporting the very basics of RSS 2.0.
+
+f: filename to read from/write to.
+title: feed title.
+link: feed website URL.
+desc: feed description.
+
+This is a dict of feed attributes ('title', 'link', etc.).
+
+    ATTRIBUTES
+
+f: as given.
+
+"""
+
+    """Feed attributes supported by this implementation."""
     SUPPORTED_ATTRS = ('title', 'link', 'desc')
 
-    def __init__ (self, f, title, link, desc, mode='update'):
+    def __init__ (self, f, title, link, desc):
         dict.__init__(self)
         self.update({'title': title, 'link': link, 'desc': desc})
-        self.mode = mode
         self.f = f
         self.create()
 
@@ -70,6 +84,11 @@ class Feed (dict):
         return i
 
     def feed_items (self):
+        """Read items from the feed.
+
+This is a generator, and each item is a dict as taken by Feed.add.
+
+"""
         i = self._read()
         parents = []
         for event, elem in i:
@@ -98,17 +117,32 @@ class Feed (dict):
             FeedWriter(f, self).end()
 
     def create (self, overwrite=False):
+        """Write an empty feed to disk.
+
+This writes general feed attributes without any items to the file specified by
+Feed.f.
+
+"""
         if (overwrite or not isinstance(self.f, str) or
             not os.path.isfile(self.f)):
             self._create()
 
     def add (self, *items):
+        """Add any number of items to the feed on disk.
+
+items: feed items, each a dict of feed item elements with string values;
+       'title' and 'description' are required.  'pubDate' may be a
+       time.struct_time, and the current local time is used if it is not
+       specified.  If 'guid' is not specified, and random UUID is used.
+       Elements requiring sub-elements are not supported.
+
+This method writes the new feed to a temporary file then replaces the original
+file with it.
+
+"""
         tmp_fd, tmp_path = tempfile.mkstemp(dir=os.path.dirname(self.f),
                                             text=True)
         try:
-            for item in self.feed_items():
-                pass
-            return
             tmp_f = os.fdopen(tmp_fd, 'w')
             feed = FeedWriter(tmp_f, self)
 
@@ -128,6 +162,13 @@ class Feed (dict):
 
 
 class FeedWriter:
+    """Write an RSS feed to disk, one item at a time.
+
+f: filename to write to.
+attrs: dict of feed attributes.
+
+"""
+
     def __init__ (self, f, attrs):
         self._ended = False
         self._feed = feed = XMLGenerator(f, 'utf-8')
@@ -144,13 +185,15 @@ class FeedWriter:
             feed.endElement(tag)
 
     def add (self, item):
+        """Add an item to the feed (dict of elements with string values)."""
         if self._ended:
-            raise Exception('tried to add item to closed FeedWriter') # TODO
+            raise Exception('tried to add item to closed FeedWriter')
         self._feed.startElement('item', Attrs({}))
         self._add_elems(item)
         self._feed.endElement('item')
 
     def end (self):
+        """Write the end of the feed."""
         if not self._ended:
             self._ended = True
             self._feed.endElement('channel')
